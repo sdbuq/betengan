c725d18583660e5936d0b932269bdf4ca4b8a224
sdbuq <sdsbuq@gmail.com>
2026-02-21 08:23:44 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
-// Service worker kosong. Cukup untuk memanipulasi browser 
-// agar menganggap ini sebagai PWA yang valid.
-self.addEventListener('fetch', function(event) {
-  // Biarkan kosong
+const CACHE_NAME = 'buqis-cache-v1';
+
+// Saat proses instalasi PWA
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+// Saat PWA diaktifkan
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Menipu Chrome agar menganggap kita punya offline support
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response('Anda sedang offline.');
+    })
+  );
 });
