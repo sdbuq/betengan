00707f34d8a6d7ff5ad5e913c1d532bf3d314557
sdbuq <sdsbuq@gmail.com>
2026-02-21 08:04:32 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+// Service worker kosong. Cukup untuk memanipulasi browser 
+// agar menganggap ini sebagai PWA yang valid.
+self.addEventListener('fetch', function(event) {
+  // Biarkan kosong
+});
